5d802458e617ccce33b9d8636825ade154c365d1
user <amathassen05@gmail.com>
2025-12-04 19:13:42 +0300

    lab3-arrays

--- src/Arrayss/Arrayss.java
+++ src/Arrayss/Arrayss.java
@@ -5,55 +5,73 @@ import java.util.Scanner;
 public class Arrayss {
     Scanner scanner = new Scanner(System.in);
 
-   public void input(int[] a) {
+    public void input(int[] a) {
         System.out.println("please enter " + a.length + " elements");
 
         for (int i = 0; i < a.length; i++) {
             a[i] = scanner.nextInt();
         }
     }
-  /* public void ReverseTraversal(int[] a){
-        System.out.println("The element in reverse are : ");
-        for(int i=a.length-1; i>=0; i--){
-            System.out.println("index["+i+"]="+a[i]);
-        }
-    }
-    public void Traversal(int[] a) {
-        System.out.println("The elements are : ");
 
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("Index[" + i + "] = " + a[i]);
-        }
-    }
-    public void Traversal2(int[] a){
-        for(int e :a){
-            System.out.println(e);
-        }
-    }
-    public void update(int[] a, int index, int nvalue){
+    /* public void ReverseTraversal(int[] a){
+          System.out.println("The element in reverse are : ");
+          for(int i=a.length-1; i>=0; i--){
+              System.out.println("index["+i+"]="+a[i]);
+          }
+      }
+      public void Traversal(int[] a) {
+          System.out.println("The elements are : ");
+
+          for (int i = 0; i < a.length; i++) {
+              System.out.println("Index[" + i + "] = " + a[i]);
+          }
+      }
+      public void Traversal2(int[] a){
+          for(int e :a){
+              System.out.println(e);
+          }
+      }
+      public void update(int[] a, int index, int nvalue){
+          if(index<0 || index>=a.length)
+              System.out.println("Index not found");
+          else
+              a[index]=nvalue;
+          for (int i = 0; i < a.length; i++) {
+              System.out.println("Index[" + i + "] = " + a[i]);
+          }
+      }
+    public void delete(int[] a, int index){
         if(index<0 || index>=a.length)
             System.out.println("Index not found");
         else
-            a[index]=nvalue;
+            a[index]=0;
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Index[" + i + "] = " + a[i]);
+        }
+    }*/
+    public void ShiftLeft_delete(int[] a, int index) {
+        for (int i = index; i < a.length - 1; i++)
+            a[i] = a[i + 1];
+        a[a.length - 1] = Integer.MIN_VALUE;
         for (int i = 0; i < a.length; i++) {
             System.out.println("Index[" + i + "] = " + a[i]);
         }
     }
-  public void delete(int[] a, int index){
-      if(index<0 || index>=a.length)
-          System.out.println("Index not found");
-      else
-          a[index]=0;
-      for (int i = 0; i < a.length; i++) {
-          System.out.println("Index[" + i + "] = " + a[i]);
-      }
-  }*/
-  public void ShiftLeft_delete(int[] a, int index){
-      for(int i=index; i<a.length-1; i++)
-          a[i]=a[i+1];
-      a[a.length-1]=Integer.MIN_VALUE;
-      for (int i = 0; i < a.length; i++) {
-          System.out.println("Index[" + i + "] = " + a[i]);
-      }
-  }
+
+    public int linearSearch(int[] a, int svalue) {
+        for (int i = 0; i < a.length; i++) {
+            if (svalue == a[i])
+                return i;
+        }
+        return -1;
+    }
+    public void ShitRight_delete(int[] a, int index) {
+        for (int i = a.length-2; i >=index ; i--)
+          a[i + 1] = a[i]  ;
+        a[index] = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Index[" + i + "] = " + a[i]);
+        }
+    }
 }
+
--- src/Arrayss/Main.java
+++ src/Arrayss/Main.java
@@ -6,12 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Arrayss arr=new Arrayss();
         int []numbers=new int[5];
+        //int []numbers ={10,20,30,40,50};
         arr.input(numbers);
         //arr.ReverseTraversal(numbers);
        // arr.Traversal(numbers);
         //arr.Traversal2(numbers);
        // arr.update(numbers, 2, 4);
         //arr.delete(numbers,1);
-        arr.ShiftLeft_delete(numbers,2);
+        //arr.ShiftLeft_delete(numbers,2);
+        arr.ShitRight_delete(numbers,1);
+        /*int result=arr.linearSearch(numbers,4);
+        if (result!= -1)
+            System.out.println("The index is found :"+result);
+        else
+            System.out.println("Nut found :");*/
+
+
     }
 }
